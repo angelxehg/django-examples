b427529c7435dd714949e1a29f67bd9f05dc4630
Angel Hurtado <contact@angelxehg.com>
2021-02-18 13:27:21 -0600

    Crear modelo Book

--- bookstore/models.py
+++ bookstore/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    """
+    Book Model class
+    This model describes a generic book 
+    """
+
+    isbn = models.CharField(max_length=15)
+    title = models.CharField(max_length=100)
+    descr = models.CharField(max_length=200, null=True, blank=True)
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return f'Book {self.title[:20]}'
